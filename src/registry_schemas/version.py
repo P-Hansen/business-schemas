1a02a238066991d49b72312c59c27fadd56f57c0
EasonPan <Eason.Pan@gov.bc.ca>
2025-06-13 15:44:04 -0700

    28460 - Update example data (#187)
    * update AR and CoD examples
    
    * update share series related fields in examples
    
    * update version
    
    * update addressCountry from Canada to CA

--- src/registry_schemas/version.py
+++ src/registry_schemas/version.py
@@ -24,7 +24,11 @@ Development release segment: .devN
 
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 __version__ = '2.18.49'  # pylint: disable=invalid-name
 =======
 __version__ = '2.18.45'  # pylint: disable=invalid-name
 >>>>>>> 4d1e707 (28913 - Update intentToLiquidate Sample Data (#185))
+=======
+__version__ = '2.18.46'  # pylint: disable=invalid-name
+>>>>>>> 40579c8 (28460 - Update example data (#187))
